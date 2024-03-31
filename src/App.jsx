5e08c52d5765bba9ecc5077e64e325e3320dde5b
alejopua <alejopua@gmail.com>
2024-03-31 14:00:53 -0500

    Refactor App component to use Redux and fetch user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Header, Email } from './components'
+import { addUser } from './redux/userSlice'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+      .then((response) => response.json())
+      .then((data) => dispatch(addUser(data)))
+      .catch((error) => console.error('Error:', error))
+  
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Email />
     </>
   )
 }
-
-export default App
