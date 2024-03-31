5bbd0cce74bc870d64e58239d4807bd230e65464
alejopua <alejopua@gmail.com>
2024-03-31 14:00:38 -0500

    Add Email component for user email input

---
+++ src/components/Email.jsx
@@ -0,0 +1,13 @@
+import { useSelector, useDispatch } from 'react-redux' 
+import { changeEmail } from '../redux/userSlice'
+
+export const Email = () => {
+  const dispatch = useDispatch();
+  const email = useSelector((state) => state.user.email)
+  const handleChange = (e) => {
+    dispatch(changeEmail(e.target.value))
+  }
+  return (
+    <input type="email" value={email} placeholder='email' onChange={handleChange} />
+  )
+}
